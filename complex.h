302d0f629ed6890506cb7476b34f86f4a7edb24e
Abhrankan Chakrabarti <abhrankan@gmail.com>
2024-05-11 16:28:06 +0530

    Update complex.h

--- complex.h
+++ complex.h
@@ -2,64 +2,32 @@
 #define COMPLEX_H
 
 #include <iostream>
-#include <cmath>
 
-using namespace std;
+class complex {
+    friend std::ostream &operator<<(std::ostream &, const complex &);
+    friend std::istream &operator>>(std::istream &, complex &);
 
-class Complex {
 private:
-    double real;
-    double imaginary;
+    char drg, *u;
+    double x, y;
 
 public:
-    Complex(double r = 0, double i = 0) : real(r), imaginary(i) {}
-
-    double getReal() const { return real; }
-    double getImaginary() const { return imaginary; }
-
-    void setReal(double r) { real = r; }
-    void setImaginary(double i) { imaginary = i; }
-
-    double modulus() const {
-        return sqrt(real * real + imaginary * imaginary);
-    }
-
-    double argument() const {
-        return atan2(imaginary, real);
-    }
-
-    Complex conjugate() const {
-        return Complex(real, -imaginary);
-    }
-
-    Complex reciprocal() const {
-        double mod = modulus();
-        return Complex(real / (mod * mod), -imaginary / (mod * mod));
-    }
-
-    Complex operator+(const Complex& other) const {
-        return Complex(real + other.real, imaginary + other.imaginary);
-    }
-
-    Complex operator-(const Complex& other) const {
-        return Complex(real - other.real, imaginary - other.imaginary);
-    }
-
-    Complex operator*(const Complex& other) const {
-        return Complex(real * other.real - imaginary * other.imaginary,
-                       real * other.imaginary + imaginary * other.real);
-    }
-
-    Complex operator/(const Complex& other) const {
-        double mod = other.modulus();
-        return Complex((real * other.real + imaginary * other.imaginary) / (mod * mod),
-                       (imaginary * other.real - real * other.imaginary) / (mod * mod));
-    }
-
-    friend ostream& operator<<(ostream& os, const Complex& complex) {
-        os << complex.real << " + " << complex.imaginary << "i";
-        return os;
-    }
+    bool p = false;
+    void angle(bool from_rad);
+    static double convert(double i, char u, bool from_rad);
+    static double convert(double i, char u);
+    static char *unit(char u);
+    double mod();
+    double arg();
+    complex conjugate();
+    complex reci();
+    complex pol(char u);
+    complex rec();
+    complex operator+(complex &m);
+    complex operator-(complex &m);
+    complex operator*(complex &m);
+    complex operator/(complex &m);
+    complex operator^(complex &m);
 };
 
-#endif /* COMPLEX_H */
+#endif
