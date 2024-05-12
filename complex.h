79c52f4c501b857cb86adc5db64fb764421a9542
Abhrankan Chakrabarti <abhrankan@gmail.com>
2024-05-12 06:42:49 +0530

    Update complex.h

--- complex.h
+++ complex.h
@@ -1,33 +1,39 @@
 #ifndef COMPLEX_H
 #define COMPLEX_H
 
-#include <iostream>
+class complex
+{
+  private:
+	static char drg;
 
-class complex {
-    friend std::ostream &operator<<(std::ostream &, const complex &);
-    friend std::istream &operator>>(std::istream &, complex &);
+	void angle(bool);
 
-private:
-    char drg, *u;
-    double x, y;
+  public:
+	double x, y;
+	static char *u;
+	static bool p;
+	complex(double real = 0, double imag = 0) : x(real), y(imag) {}
 
-public:
-    bool p = false;
-    void angle(bool from_rad);
-    static double convert(double i, char u, bool from_rad);
-    static double convert(double i, char u);
-    static char *unit(char u);
-    double mod();
-    double arg();
-    complex conjugate();
-    complex reci();
-    complex pol(char u);
-    complex rec();
-    complex operator+(complex &m);
-    complex operator-(complex &m);
-    complex operator*(complex &m);
-    complex operator/(complex &m);
-    complex operator^(complex &m);
+	static double convert(double, char, bool = true);
+
+	static char *unit(char);
+
+	double mod();
+	double arg();
+
+	complex conjugate();
+	complex reci();
+	complex pol(char = 'd');
+	complex rec();
+
+	complex operator+(complex &);
+	complex operator-(complex &);
+	complex operator*(complex &);
+	complex operator/(complex &);
+	complex operator^(complex &);
+
+	friend std::ostream &operator<<(std::ostream &, const complex &);
+	friend std::istream &operator>>(std::istream &, complex &);
 };
 
 #endif
